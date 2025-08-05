3008bc66571d66c495b16c28f18d0c41f324cfdb
nuttyCream <issanutty@gmail.com>
2025-08-05 04:22:14 -0700

    feat: impl ValidArgs for available completion script shells

--- cmd/completion/completion.go
+++ cmd/completion/completion.go
@@ -9,17 +9,25 @@ import (
 
 func CompletionCommand() *cobra.Command {
 	cmd := cobra.Command{
-		Use:                   "completion {bash|zsh|fish}",
+		Use:                   "completion {bash|zsh|fish|nushell|elvish|xonsh|powershell}",
 		Short:                 "Generate completion scripts",
 		Long:                  "Generate completion scripts for use in shells.",
 		Hidden:                true,
 		DisableFlagsInUseLine: true,
-		ValidArgs:             []string{},
-		Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+		ValidArgs: []string{
+			"bash",
+			"zsh",
+			"fish",
+			"nushell",
+			"elvish",
+			"powershell",
+			"xonsh",
+		},
+		Args: cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 		Run: func(cmd *cobra.Command, args []string) {
 			completion_script, err := carapace.Gen(cmd.Root()).Snippet(args[0])
 			if err != nil {
-				fmt.Println("", err)
+				fmt.Println(err)
 				return
 			}
 			fmt.Println(completion_script)
