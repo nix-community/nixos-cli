69ed3b7304d5106d0e7fbb48b70ce14bfd06a940
nuttyCream <issanutty@gmail.com>
2025-08-05 14:29:43 -0700

    fix: rm powershell from valid args + use Fprintln for printing errors

--- cmd/completion/completion.go
+++ cmd/completion/completion.go
@@ -2,6 +2,7 @@ package completion
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/carapace-sh/carapace"
 	"github.com/spf13/cobra"
@@ -9,7 +10,7 @@ import (
 
 func CompletionCommand() *cobra.Command {
 	cmd := cobra.Command{
-		Use:                   "completion {bash|zsh|fish|nushell|elvish|xonsh|powershell}",
+		Use:                   "completion {bash|zsh|fish|nushell|elvish|xonsh}",
 		Short:                 "Generate completion scripts",
 		Long:                  "Generate completion scripts for use in shells.",
 		Hidden:                true,
@@ -20,14 +21,13 @@ func CompletionCommand() *cobra.Command {
 			"fish",
 			"nushell",
 			"elvish",
-			"powershell",
 			"xonsh",
 		},
 		Args: cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 		Run: func(cmd *cobra.Command, args []string) {
 			completion_script, err := carapace.Gen(cmd.Root()).Snippet(args[0])
 			if err != nil {
-				fmt.Println(err)
+				fmt.Fprintln(os.Stderr, "error: failed to generate scripts:", err)
 				return
 			}
 			fmt.Println(completion_script)
