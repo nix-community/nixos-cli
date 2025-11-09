08ca12fa652752da0f87c5feabec1a899500a456
Varun Narravula <varun@snare.dev>
2025-11-09 04:13:56 -0800

    chore: re-add dev suffix to version

--- package.nix
+++ package.nix
@@ -10,7 +10,7 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "nixos-cli";
-  version = "0.15.0";
+  version = "0.15.0-dev";
   src = nix-gitignore.gitignoreSource [] ./.;
 
   vendorHash = "sha256-I04cLEXLTRbxxCBemj4J6JzrtDaUQS0TuZZ8Fa0EZBk=";
