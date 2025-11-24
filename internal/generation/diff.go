32f9e8492e1adf2e39d8e79c40ce2027c1526e51
Varun Narravula <varun@snare.dev>
2025-11-23 23:19:17 -0800

    refactor(settings): add diff tool section, deprecate use_nvd option

--- internal/generation/diff.go
+++ internal/generation/diff.go
@@ -3,30 +3,55 @@ package generation
 import (
 	"os/exec"
 
+	"github.com/nix-community/nixos-cli/internal/settings"
 	"github.com/nix-community/nixos-cli/internal/system"
 )
 
 type DiffCommandOptions struct {
-	UseNvd bool
+	DiffTool    settings.DiffTool
+	DiffToolCmd []string
 }
 
-func RunDiffCommand(s system.CommandRunner, before string, after string, opts *DiffCommandOptions) error {
+func RunDiffCommand(s system.System, before string, after string, opts *DiffCommandOptions) error {
 	log := s.Logger()
 
-	useNvd := opts.UseNvd
+	tool := opts.DiffTool
 
-	if opts.UseNvd {
-		nvdPath, _ := exec.LookPath("nvd")
-		nvdFound := nvdPath != ""
-		if !nvdFound {
-			log.Warn("use_nvd is specified in config, but `nvd` is not executable")
-			log.Warn("falling back to `nix store diff-closures`")
-			useNvd = false
+	switch tool {
+	case settings.DifferInternal:
+		if s.IsRemote() {
+			log.Warn("the internal differ does not work with remote systems")
+			tool = settings.DifferNix
+		}
+	case settings.DifferCommand:
+		cmd := opts.DiffToolCmd[0]
+		if cmdPath, _ := exec.LookPath(cmd); cmdPath == "" {
+			log.Warnf("differ.command uses '%s', but `%s` is not executable", cmd, cmd)
+			tool = settings.DifferNix
+		}
+	case settings.DifferNvd:
+		if nvdPath, _ := exec.LookPath("nvd"); nvdPath == "" {
+			log.Warn("differ.tool is set to 'nvd', but `nvd` is not executable")
+			tool = settings.DifferNix
 		}
 	}
 
-	argv := []string{"nix", "store", "diff-closures", before, after}
-	if useNvd {
+	if opts.DiffTool != tool && tool == settings.DifferNix {
+		log.Warn("falling back to `nix store diff-closures`")
+	}
+
+	var argv []string
+
+	switch tool {
+	case settings.DifferCommand:
+		argv = append(opts.DiffToolCmd, before, after)
+	case settings.DifferInternal:
+		// TODO : implement diff
+		log.Info("internal differ currently not implemented")
+		return nil
+	case settings.DifferNix:
+		argv = []string{"nix", "store", "diff-closures", before, after}
+	case settings.DifferNvd:
 		argv = []string{"nvd", "diff", before, after}
 	}
 
