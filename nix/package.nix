82b90d5f30d2729bcb71b7b566137a24f884b865
Varun Narravula <varun@snare.dev>
2026-02-21 16:06:55 -0800

    fix(package): add nix from pkgs set to propagatedBuildInputs

--- nix/package.nix
+++ nix/package.nix
@@ -4,6 +4,7 @@
   installShellFiles,
   stdenv,
   scdoc,
+  nix,
   revision ? "unknown",
   flake ? true,
 }:
@@ -27,6 +28,18 @@ buildGoModule (finalAttrs: {
   vendorHash = "sha256-J4vibcWXzeBInb4CdNmqP8svYF2QX0Gccm/kiumQ4rA=";
 
   nativeBuildInputs = [installShellFiles scdoc];
+
+  propagatedBuildInputs = [
+    # Make sure that we use the Nix package we depend on, not something
+    # else from the PATH for nix-{env,instantiate,build}. This is
+    # important, because NixOS defaults the architecture of the rebuilt
+    # system to the architecture of the nix-* binaries used. So if on an
+    # amd64 system the user has an i686 Nix package in her PATH, then we
+    # would silently downgrade the whole system to be i686 NixOS on the
+    # next reboot.
+    # Credit where credit is due: this note is taken directly from nixos-rebuild-ng.
+    (lib.getBin nix)
+  ];
 
   env = {
     CGO_ENABLED = 0;
