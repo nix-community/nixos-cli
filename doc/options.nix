403345e4a63778c391dab61f83fdebc3fce20c50
Varun Narravula <varun@snare.dev>
2026-03-01 15:47:01 -0800

    fix(docs): do not use impure pkgs reference when evaluating docs

--- doc/options.nix
+++ doc/options.nix
@@ -1,7 +1,8 @@
-{pkgs ? import <nixpkgs> {}}: let
+let
   self = (import ../nix/flake-compat.nix).outputs;
 
   inherit (self.inputs) nixpkgs;
+  pkgs = nixpkgs.legacyPackages.${builtins.currentSystem};
 
   nixosModules = import "${nixpkgs}/nixos/modules/module-list.nix";
 
@@ -14,11 +15,7 @@ in
         self.nixosModules.nixos-cli
       ];
     specialArgs = {
-      # This missing package is preventing option evaluation from
-      # occurring. This needs to be fixed upstream.
-      pkgs = pkgs.extend (final: prev: {
-        xf86-video-nested = prev.xf86_video_nested;
-      });
+      inherit pkgs;
     };
     excluded = [
       "_module.args"
